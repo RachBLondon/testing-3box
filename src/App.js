dafd33bd8cf9c5d489c6701f7784d96ef419aa79
Rachel Black <rachblondon@gmail.com>
2019-11-13 18:21:34 +0100

    basic comments

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React, { Component } from "react";
-import QR from './demo-qr.png';
+import QR from "./demo-qr.png";
+
+import Box from "3box";
+import ThreeBoxComments from "3box-comments-react";
 import ProfileHover from "profile-hover";
 
 
-import Box from "3box";
-
-
-const getThreeBox = async address => {
-  const profile = await Box.getProfile(address);
-  console.log(profile);
-  return profile;
-};
+const spaceName = "testing3Boxyeah!!";
 
 export default class App extends Component {
   state = {
     needToAWeb3Browser: false
   };
+
   async getAddressFromMetaMask() {
     if (typeof window.ethereum == "undefined") {
       this.setState({ needToAWeb3Browser: true });
@@ -25,20 +22,29 @@ export default class App extends Component {
       this.setState({ accounts });
     }
   }
+
   async componentDidMount() {
     await this.getAddressFromMetaMask();
     if (this.state.accounts) {
-      const threeBox = await getThreeBox(this.state.accounts[0]);
-      this.setState({ threeBox });
+      const openedBox = await Box.openBox(
+        this.state.accounts[0],
+        window.ethereum
+      );
+      this.setState({ openedBox });
+      const space = await this.state.openedBox.openSpace(spaceName);
+      this.setState({ space });
     }
   }
+
   render() {
     if (this.state.needToAWeb3Browser) {
       return <h1>Please install metamask</h1>;
     }
 
+    const admin = "0x2f4cE4f714C68A3fC871d1f543FFC24b9b3c2386";
+
     return (
-      <div style={{ width : "400px", margin : "auto", textAlign : "center"}}>
+      <div style={{ width: "400px", margin: "auto", textAlign: "center" }}>
         <h1>3Box Demo</h1>
         {this.state.needToAWeb3Browser && <h3>Use a Web3 Enabled Browser</h3>}
         {this.state.accounts && (
@@ -50,6 +56,17 @@ export default class App extends Component {
           </div>
         )}
         <img src={QR} />
+        {this.state.accounts && this.state.openedBox && (
+          <ThreeBoxComments
+            // required
+            spaceName={spaceName}
+            threadName="myThreadName"
+            adminEthAddr={admin}
+            // Required props for context A) & B)
+            box={this.state.openedBox}
+            currentUserAddr={this.state.accounts[0]}
+          />
+        )}
       </div>
     );
   }
