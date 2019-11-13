65d6022d22e3d94e0020f7c9775d47ecf0243b9f
Rachel Black <rachblondon@gmail.com>
2019-11-13 18:30:34 +0100

    centering

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Box from "3box";
 import ThreeBoxComments from "3box-comments-react";
 import ProfileHover from "profile-hover";
 
-
 const spaceName = "testing3Boxyeah!!";
 
 export default class App extends Component {
@@ -37,25 +36,33 @@ export default class App extends Component {
   }
 
   render() {
+    const admin = "0x2f4cE4f714C68A3fC871d1f543FFC24b9b3c2386";
     if (this.state.needToAWeb3Browser) {
       return <h1>Please install metamask</h1>;
     }
-
-    const admin = "0x2f4cE4f714C68A3fC871d1f543FFC24b9b3c2386";
 
     return (
       <div style={{ width: "400px", margin: "auto", textAlign: "center" }}>
         <h1>3Box Demo</h1>
         {this.state.needToAWeb3Browser && <h3>Use a Web3 Enabled Browser</h3>}
         {this.state.accounts && (
-          <div>
+          <div
+            style={{
+              display: "flow-root",
+              display: "flowRoot",
+              width: "177.516px",
+              margin: "auto"
+            }}
+          >
             <threebox-address
               data-address={this.state.accounts[0]}
             ></threebox-address>
             <ProfileHover address={this.state.accounts[0]} />
           </div>
         )}
-        <img src={QR} />
+        <div>
+          <img src={QR} />
+        </div>
         {this.state.accounts && this.state.openedBox && (
           <ThreeBoxComments
             // required
